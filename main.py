56b43ce965ee1d8f6b501229ece531121c3e3de3
AkashTyagi02 <akkutyagi2002@gmail.com>
2025-05-25 02:26:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("how are you doing?")
+print(8)
+print("my name is akash tyagi")
+print(8*9)
+
+print ("hey my name is akash \n and my brother name is kartikey tyagi")# \n is used to print in next line 
+print(" 0 is a natural number") #dont use 0 as a number #this is a comment  
+
+'''
+hey i am akash 
+hey my name is kartikey 
+hey i am gajendar 
+hey i am krishna
+this is a multiline comment 
+# this is a single line comment
+double quotes are also used in comments """
+'''
+
+print("hey are you a \"good boy\" \n or a \"bad boy\"")# \"# is used to print " in the string and is used to show in comas 
+
+print("you","and","are","good","friends.", sep="~")# sep is used to separate the strings with the given character
+
+print("you","and","are","good","friends.",sep="~", end="\n no.1")# end is used to end the string with the given character
+print("akku")
